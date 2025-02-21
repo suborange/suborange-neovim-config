479196026c33de709ba592ba7e87a4652a955673
suborange <ebonavida0@gmail.com>
2025-02-20 16:45:58 -0800

    fixed formatoptions

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,18 @@
 -- init.lua requirements
-require('options')
-require('keymaps')
-require('plugins')
+-- 
+require 'options'
+require 'keymaps'
+require 'plugins'
 
 vim.cmd("colorscheme koehler")
+
+vim.cmd[[
+augroup FormatOptions
+autocmd!
+autocmd FileType * setlocal formatoptions -=c
+autocmd FileType * setlocal formatoptions -=r
+augroup END
+]]
 foo = function() 
 	print("testing colors") end
 
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,11 +3,15 @@ termguicolors = true, -- set term gui colors
 }
 
 local opt = vim.opt;
+opt.termguicolors = true;
  -- basic editor options 
 opt.number = true;
 opt.tabstop = 2;
 opt.shiftwidth = 2;
 opt.expandtab = true; -- okay so tab stop seems to work when expandtab is false
 -- formatoptions [:set formatoptions?] - remove(), append()
-opt.formatoptions:remove('c') -- should disbale auto comments 
-opt.formatoptions:remove('r') -- should disbale auto comments one new lines
+--opt.formatoptions:remove('c') -- should disbale auto comments 
+--opt.formatoptions:remove('r') -- should disbale auto comments one new lines
+
+-- terminal stuff
+opt.clipboard = 'unnamedplus'
